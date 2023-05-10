1804288057ba8f1fe659458b25162b441fba39a4
oleksii-yermolaiev <s25653@pjwstk.edu.pl>
2023-05-10 14:10:43 +0200

    Create lab9/zad1.py

---
+++ lab9/zad1.py
@@ -0,0 +1,8 @@
+def print_name_and_age(name, age):
+    print("Imie: " + name + ", age: " + age)
+
+
+if __name__ == "__main__":
+    name = input("Podaj imie: ")
+    age = int(input("Podaj wiek: "))
+    print_name_and_age(name, age)
